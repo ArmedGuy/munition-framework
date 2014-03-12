7286b2886cc3ec61c7d4777bd347e2b693af4849
Johan Jatko <johan@pie-studios.com>
2014-03-12 01:13:55 +0100

    Dynamic path

--- app/templates/header.php
+++ app/templates/header.php
@@ -2,10 +2,10 @@
  <html lang="en">
   <head>
     <meta charset="utf-8" />
-    <link rel="stylesheet" type="text/css" href="/munition-framework/app/public/css/bootstrap.min.css" />
-    <link rel="stylesheet" type="text/css" href="/munition-framework/app/public/css/style.css" />
+    <link rel="stylesheet" type="text/css" href="<?=MUNITION_WEBPATH;?>app/public/css/bootstrap.min.css" />
+    <link rel="stylesheet" type="text/css" href="<?=MUNITION_WEBPATH;?>app/public/css/style.css" />
     
-    <script type="text/javascript" src="/munition-framework/app/public/js/jquery.min.js"></script>
+    <script type="text/javascript" src="<?=MUNITION_WEBPATH;?>app/public/js/jquery.min.js"></script>
     
     <title>Munition Framework</title>
   </head>
@@ -13,7 +13,7 @@
     <nav class="navbar navbar-default navbar-static-top" role="navigation">
       <div class="container">
         <div class="navbar-header">
-          <a class="navbar-brand" href="#"><img id="munition-logo" src="/munition-framework/app/public/img/munition-logo.png" alt="Logo"/> Munition Framework</a>
+          <a class="navbar-brand" href="#"><img id="munition-logo" src="<?=MUNITION_WEBPATH;?>app/public/img/munition-logo.png" alt="Logo"/> Munition Framework</a>
         </div>
       </div>
     </nav>
