8dc6c9555e80a3aa9cbd7440f386571b1401104c
Johan Jatko <johan@pie-studios.com>
2014-03-13 01:45:54 +0100

    Woops, those weren't supposed to be there.

--- config/routes.php
+++ config/routes.php
@@ -4,12 +4,9 @@ class AppRouter extends \framework\base\Router {
   function __construct() {
     parent::__construct();
     
-    $this->base = "/munition-framework";
-    
     $this->get("/", "install#home");
     $this->error("404", "install#home");
     $this->get("/verify_rewrite", "install#verify_rewrite");
-    $this->get("/server_info", "install#server_info");
   
   }
 }
