257897c5fc36874bcc9cf4ba8f84db55402f618e
Johan Jatko <johan@pie-studios.com>
2014-03-14 18:06:41 +0100

    Started on DB Migration

---
+++ framework/db/db_migration.php
@@ -0,0 +1,20 @@
+<?php
+namespace framework\db;
+class DbMigration {
+  public function up() {
+  }
+  public function down() {
+  }
+  public function change() {
+  }
+  
+  protected function create_table($name, $structure) {
+  }
+  protected function delete_table() {
+  }
+  protected function add_column() {
+  }
+  protected function remove_column() {
+  }
+  
+}
--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 require_once 'framework/munition.php';
-include_once 'app/models/User.php';
 
 $app = new \framework\base\App("./app/", "./config/routes.php");
 $app->run();
