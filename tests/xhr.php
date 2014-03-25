65483343d92bf620d813a4514458f11b5533e891
Johan Jatko <johan@pie-studios.com>
2014-03-25 12:08:50 +0100

    XHR response for reading data

--- tests/xhr.php
+++ tests/xhr.php
@@ -6,7 +6,19 @@ $_SERVER["SCRIPT_FILENAME"] = $_SERVER["DOCUMENT_ROOT"] . "/index.php";
 $_SERVER["REMOTE_ADDR"] = "127.0.0.1";
 $_SERVER["REMOTE_PORT"] = 80;
 // simulates HTTP request
+$_xhr_request_made = false;
 function xhr($path, $method) {
+  $_xhr_request_made = true;
+  ob_start();
   $_SERVER["REQUEST_URI"] = $path;
   $_SERVER["REQUEST_METHOD"] = $method;
+}
+
+function xhr_response() {
+  if(!$_xhr_request_made) {
+    throw new Exception("No request was made, unable to get request");
+  } else {
+    $_xhr_request_made = true;
+    return [http_response_code(), get_headers($), ob_get_clean()];
+  }
 }
