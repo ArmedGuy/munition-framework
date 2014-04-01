681847e8ec7de169f300a542c9f572c778a11d07
Johan Jatko <johan@pie-studios.com>
2014-04-01 11:04:10 +0200

    Test formatting in Lclzr

--- tests/framework/lclzr/LocalizerTest.php
+++ tests/framework/lclzr/LocalizerTest.php
@@ -7,4 +7,11 @@ class LocalizerTest extends PHPUnit_Framework_TestCase {
     $str = L::t("user.new");
     $this->assertEquals("Create User", $str);
   }
+  
+  public function testStringFormatting() {
+    L::loadFolder("./tests/test_config/locales/");
+    L::$current = "en";
+    $str = L::t("user.hi", "ArmedGuy");
+    $this->assertEquals("Hi ArmedGuy!", $str);
+  }
 }
