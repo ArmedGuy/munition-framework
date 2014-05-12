35ccc21b054e6978cb76fe8565c0d655df400e9a
Johan Jatko <johan@pie-studios.com>
2014-05-12 22:12:25 +0200

    Fixed LocalizerTest referencing old config path

--- tests/framework/lclzr/LocalizerTest.php
+++ tests/framework/lclzr/LocalizerTest.php
@@ -2,27 +2,27 @@
 use \Lclzr\Locale as L;
 class LocalizerTest extends PHPUnit_Framework_TestCase {
   public function testLoadAndShowLocale() {
-    L::loadFolder("./tests/test_config/locales/");
+    L::loadFolder("./tests/test_app/locales/");
     L::$current = "en";
     $str = L::t("user.new");
     $this->assertEquals("Create User", $str);
   }
   
   public function testStringFormatting() {
-    L::loadFolder("./tests/test_config/locales/");
+    L::loadFolder("./tests/test_app/locales/");
     L::$current = "en";
     $str = L::t("user.hi", "ArmedGuy");
     $this->assertEquals("Hello ArmedGuy!", $str);
   }
   
   public function testUnknownString() {
-    L::loadFolder("./tests/test_config/locales/");
+    L::loadFolder("./tests/test_app/locales/");
     L::$current = "en";
     $str = L::t("swag");
     $this->assertEquals("{Unknown locale: swag}", $str);
   }
   public function testMergeDefinition() {
-    L::loadFolder("./tests/test_config/locales/");
+    L::loadFolder("./tests/test_app/locales/");
     L::$current = "en";
     $str = L::t("user.new");
     $this->assertEquals("Create User", $str);
