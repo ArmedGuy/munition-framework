7fc8b0c634c998adbec91cdbaae1ce85b85f6729
Johan Jatko <johan@pie-studios.com>
2014-05-13 09:17:27 +0200

    More descriptive MunitionException

--- framework/munition_exception.php
+++ framework/munition_exception.php
@@ -5,5 +5,8 @@ class MunitionException extends Exception {
       $errstr .= " in '" . $errfile . " (line ". $errline.")'";
     }
     parent::__construct($errstr, $errno);
+    
+    if($errfile != null) $this->file = $errfile;
+    if($errline != 0) $this->line = $errline;
   }
 }
