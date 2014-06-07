80835b3435039a401e1b3474b08ae835c27258a4
Johan Jatko <johan.jatko@outlook.com>
2014-06-07 03:25:01 +0200

    Changed some names

--- tests/framework/Munition/DbModel/DbModelTest.php
+++ tests/framework/Munition/DbModel/DbModelTest.php
@@ -1,8 +1,8 @@
 <?php
-require_once './tests/test_app/models/user.php';
-require_once './tests/test_app/models/post.php';
-require_once './tests/test_app/models/group_permission.php';
-require_once './tests/test_app/models/group.php';
+require_once './tests/test_app/models/User.php';
+require_once './tests/test_app/models/Post.php';
+require_once './tests/test_app/models/GroupPermission.php';
+require_once './tests/test_app/models/Group.php';
 
 class DbModelTest extends PHPUnit_Framework_TestCase {
 
